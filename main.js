bbd278a312ad6c40462289cd81543e9574837907
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2022-07-02 13:53:40 +0500

    js console

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 var userName = prompt("Ismingiz: ");
 var userLastName = prompt("Familiangiz: ");
 var userStudy = prompt("Qayerda tahsil olasiz: ");
-var userAge = prompt("Tug'ulgan yilingizni kiriting: ")
+var userAge = prompt("Tug'ulgan yilingizni kiriting: ");
+var userEmail = prompt("Email manzillingizni kiriting: ");
+var userNum = prompt("Telefon Raqamingizni Kiriting: ");
 
 alert(`${"Sizning Ismingiz: " + userName}
 ${"Sizning Familiangiz: " + userLastName}
 ${"Sizning O'qish joyingiz: " + userStudy}
-${"Sizning Yoshingiz: " + (2022 - userAge)}`);
+${"Sizning Yoshingiz: " + (2022 - userAge)}
+${"Sizning Email manzilingiz: " + userEmail}
+${"Telefon Raqamingiz: " + userNum}`);
 
 console.log(`${"Sizning Ismingiz: " + userName}
 ${"Sizning Familiangiz: " + userLastName}
 ${"Sizning O'qish joyingiz: " + userStudy}
-${"Sizning Yoshingiz: " + (2022 - userAge)}`);
+${"Sizning Yoshingiz: " + (2022 - userAge)}
+${"Sizning Email manzilingiz: " + userEmail}
+${"Telefon Raqamingiz: " + userNum}`);

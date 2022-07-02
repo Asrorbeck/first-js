02cdc39cc3df6b1ed1437898eaf5c354be63a54f
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2022-07-02 13:47:28 +0500

    first js

---
+++ main.js
@@ -0,0 +1,14 @@
+var userName = prompt("Ismingiz: ");
+var userLastName = prompt("Familiangiz: ");
+var userStudy = prompt("Qayerda tahsil olasiz: ");
+var userAge = prompt("Tug'ulgan yilingizni kiriting: ")
+
+alert(`${"Sizning Ismingiz: " + userName}
+${"Sizning Familiangiz: " + userLastName}
+${"Sizning O'qish joyingiz: " + userStudy}
+${"Sizning Yoshingiz: " + (2022 - userAge)}`);
+
+console.log(`${"Sizning Ismingiz: " + userName}
+${"Sizning Familiangiz: " + userLastName}
+${"Sizning O'qish joyingiz: " + userStudy}
+${"Sizning Yoshingiz: " + (2022 - userAge)}`);
